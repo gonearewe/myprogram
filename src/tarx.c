e52992f93f1571cfbf5b0d669d5c6ed19a4f8dd6
John Mactavish <cristmactavish@outlook.com>
2019-06-10 23:35:10 +0800

    Create tarx.c

---
+++ src/tarx.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+int main(int argc,char **argv)
+{
+    if(argc==2)
+    {
+        
+    }
+    else 
+    {
+        printf("\ntarx Usage :\n");
+        printf("        tarx <filename>\n");
+    }
+    
+}
