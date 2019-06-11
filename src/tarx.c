04a98963e0d83901de2885d2dd0703a056dc752b
John Mactavish <cristmactavish@outlook.com>
2019-06-11 17:02:42 +0800

    finish tarx.c
    tarx.c tarx and stdheader.h

--- src/tarx.c
+++ src/tarx.c
@@ -1,9 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main(int argc,char **argv)
 {
     if(argc==2)
     {
+        char *p=0;
+        if(strstr(argc[1],".tar.gz"!=0)
+        {
+            sprintf(p,"tar -zxvf %s",argv[1]);
+            system(p);
+        }
+        else if (strstr(argc[1],".tar.bz2"!=0)
+        {
+            sprintf(p,"tar -jxvf %s",argv[1]);
+            system(p);
+        }
+        else
+        {
+            printf("\ntarx only supports files ended with \".tar.gz\" or \".tar.bz2\"\n");
+        }
         
     }
     else 
